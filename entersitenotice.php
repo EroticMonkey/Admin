1cd2abde540aff18e2b820a7c64c6653132e5fb5
[Adebayo lekan] <[lekan5058@gmail.com]>
2025-10-17 21:40:21 +0100

    first commit

---
+++ entersitenotice.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<style>
+body {font-family:arial, sans-serif; font-size:12px; padding:0; margin:0; }
+.ent_header {background:#EEEEEE ;font-size:22px; padding: 15px 10px;}
+.ent_content { padding: 15px 10px; color:#333; }
+img { float:right; max-width:200px; margin:-10px 20px 0 0; }
+@media only screen and (max-width: 600px) {
+	.ent_header {font-size:16px;}
+	img { max-width:150px; margin:-5px 5px 5px 0; }
+}
+@media only screen and (max-width: 400px) {
+	img { margin:30px 0 5px 5px; }
+}
+</style>
+</head>
+<body>
+<div class="ent_header">You're about to enter Erotic Monkey <img src="images/logo.png"></div>
+<div class="ent_content">
+
+
+<p>Erotic Monkey is the premiere destination for escort reviews and information.</p>
+<p>This site is purely for entertainment purposes. We imply nothing else. This site contains no nude pictures, but it does have reviews with pointers to pages that may house nude pictures. This site does contain explicit language. Due to these points, we must insist that you need to be of a legal age to view pornographic material. The reviews contained in this site are in no manner intended to appeal to the prurient interest of the average person, applying contemporary community standards. The writing contained in this site is in no manner intended to depict or describe, in a patently ofensive way, sexual conduct specifically defined by the applicable state law. All information presented at this site is protected by the First Amendment of the United States Constitution. This site is not intended for solicitation. We are not associated with anybody reviewed on this site. We do not receive any financial recompense from the ladies reviewed. These ladies are professionals, any money paid to them is for time and companionship only. Anything else that occurs is a matter of choice between consenting adults. Prostitution is illegal. It is illegal to sell or buy sexual services. You can give it away for free, but you can't sell it. Therefore, everything contained here is fiction and is for your entertainment and amusement only. If persons under 18 years of age have access to the Internet through your computer, please monitor the person's use of the Internet. By CLICKING below, you are agreeing that your local community permits you to view this web site. You are over 18 years of age. You are knowingly, intelligently, and voluntarily consent to view this site, and you have instituted an expective method of monitoring the use the Internet by persons under 18 years of age.</p>
+    <p>By entering, you agree to Erotic Monkey's "<a href="/terms" target="_blank">Terms of Service</a>" and "<a href="/privacy" target="_blank">Privacy Policy</a>".</p>
+
+<a target="_blank" href="https://www.eroticmonkey.ch/law-enforcement">Law Enforcement Guide</a>
+
+</div>
+
+
+</body></html>
